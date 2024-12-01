430ad493187ddcc71c46e84987ad3d20e564b533
Kapil Chaudhary <Kapilchaudhary706@gmail.com>
2024-12-01 17:34:27 +0530

    adding Serialization and desrialization test for json

--- CashCardApp/src/main/java/com/example/cashcardapp/CashCard.java
+++ CashCardApp/src/main/java/com/example/cashcardapp/CashCard.java
@@ -1,4 +1,21 @@
 package com.example.cashcardapp;
 
-public record CashCard(Long id, Double amount) {
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public record CashCard (Long id, Double amount){
+
+//    @JsonCreator
+//    public CashCard(
+//            @JsonProperty("id") Long id,
+//            @JsonProperty("amount") Double amount)
+//           {
+//        this.id = id;
+//        this.amount = amount;
+//        }
+
+
+
 }
--- CashCardApp/src/test/java/com/example/cashcardapp/CashCardJsonTest.java
+++ CashCardApp/src/test/java/com/example/cashcardapp/CashCardJsonTest.java
@@ -5,12 +5,15 @@ package com.example.cashcardapp;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @JsonTest
 public class CashCardJsonTest {
@@ -34,6 +37,47 @@ public class CashCardJsonTest {
 
 
     }
+
+/*
+    @Test
+    void cashCardDeserializationTest() throws IOException{
+
+
+
+//        String expected = """
+//                "id" : 99,
+//                "amount" : 123.45
+//                """;
+//
+//        assertThat(json.parse(expected))
+//                .isEqualTo(new CashCard(99L, 123.45));
+//        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+//        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Register Java 16+ module for records
+        mapper.findAndRegisterModules();
+
+        String expected2 = """
+                "id" : 99,
+                "amount" : 123.45
+                """;
+
+//        String jsonn = "{\"id\":99,\"amount\":123.45}";
+        CashCard cashCard = mapper.readValue(expected2, CashCard.class);
+//
+        assertEquals(99L, cashCard.id());
+        assertEquals(123.45, cashCard.amount());
+        
+        /* not resloving issue --
+
+        com.fasterxml.jackson.databind.exc.MismatchedInputException: Cannot construct instance of `com.example.cashcardapp.CashCard` (although at least one Creator exists): no String-argument constructor/factory method to deserialize from String value ('id')
+ at [Source: REDACTED (`StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION` disabled); line: 1, column: 1]
+         */
+
+//}
 
 }
 
